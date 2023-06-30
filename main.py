5318b1b712c5b25b26712fb474ae210765cc2e02
George Green <iamkarlson@gmail.com>
2023-06-30 13:20:22 +0200

    bot basic commands

--- main.py
+++ main.py
@@ -2,23 +2,47 @@ import json
 from pprint import pprint
 
 import functions_framework
-from telegram import Update, Bot
+from telegram import Update, Bot, Message
 from flask import Request, abort
 import os
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 bot = Bot(token=BOT_TOKEN)
 
+webhook_url = os.environ["WEBHOOK_URL"]
 
-def handle_message(message):
+
+def send_back(message: Message, text):
+    """
+    Sends a message back to the user. Using telegram bot's sendMessage method.
+    :param message: incoming telegram message
+    :param text:
+    :return:
+    """
+    bot.send_message(
+        chat_id=message.chat_id,
+        text=text
+    )
+
+
+def handle_message(message: Message):
     """
     Command handler for telegram bot.
     """
     pprint(message)
     if message.text == "/start":
-        return "Hello, World!"
+        send_back(message, "Hello brain!")
+    elif message.text == "/webhook":
+        register_webhook = bot.set_webhook(webhook_url)
+        if register_webhook:
+            send_back(message, bot.get_webhook_info().to_json())
+        else:
+            send_back(message, "Failed to register webhook")
+    elif message.text == "/info":
+        bot_info = bot.get_me()
+        send_back(message, json.dumps(bot_info.to_dict()))
     else:
-        return json.dumps(message.__dict__)
+        send_back(message, message.to_json())
 
 
 @functions_framework.http
@@ -27,18 +51,11 @@ def handle(request: Request):
     Incoming telegram webhook handler for a GCP Cloud Function.
     When request is received, body is parsed into standard telegram message model, and then forwarded to command handler.
     """
-    # when get is called, automatically register webhook to telegram using bot token from secret
-    if request.method == "GET":
-        # get bot info from "https://api.telegram.org/{BOT_TOKEN}/getMe"
-        # serializing bot object to json
-        if bool(request.args.get("webhook", False)):
-            return bot.get_webhook_info().to_json()
-        bot_info = bot.get_me()
-        return json.dumps(bot_info.to_dict())
     # when post is called, parse body into standard telegram message model, and then forward to command handler
     if request.method == "POST":
         update_message = Update.de_json(request.get_json(), bot)
-        return handle_message(update_message)
+        handle_message(update_message.message)
+        return {"statusCode": 200}
 
     # Unprocessable entity
     abort(422)
